23a7a8e0b0f56676774604bfeabe5dd6a9ef029a
daruken <>
2022-10-04 10:45:28 +0900

    feat: 프로그램에서 사용할 상수 정의 - 자동차 전진 또는 멈춤의 기준(Racing) - 에러 메시지 타이틀(Message)

---
+++ src/main/java/racingcar/constant/Message.java
@@ -0,0 +1,5 @@
+package racingcar.constant;
+
+public class Message {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+}
---
+++ src/main/java/racingcar/constant/Racing.java
@@ -0,0 +1,6 @@
+package racingcar.constant;
+
+public class Racing {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+}
