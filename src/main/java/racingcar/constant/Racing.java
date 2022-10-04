d927d13281ee3ebb01598cdcc540f54ca3ac80d0
daruken <>
2022-10-04 16:27:20 +0900

    refactor - Racing 관련 상수 접근 제어자 변경 - 사용하지 않는 상수 삭제

--- src/main/java/racingcar/constant/Racing.java
+++ src/main/java/racingcar/constant/Racing.java
@@ -1,6 +1,5 @@
 package racingcar.constant;
 
 public class Racing {
-    private static final int MOVING_FORWARD = 4;
-    private static final int STOP = 3;
+    public static final int MOVING_FORWARD = 4;
 }
