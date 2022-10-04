456669efbb361504fa593f70b67b137273d27c77
daruken <>
2022-10-04 15:53:52 +0900

    refactor: CAR 이름, 시도 회수 입력 관련 메시지 추가

--- src/main/java/racingcar/constant/Message.java
+++ src/main/java/racingcar/constant/Message.java
@@ -2,4 +2,7 @@ package racingcar.constant;
 
 public class Message {
     public static final String ERROR_MESSAGE = "[ERROR]";
+    public static final String ERROR_CAR_NAME_LENGTH = "CAR 이름은 1글자 이상, 5글자 이하만 가능합니다.";
+    public static final String INPUT_RACE_CARS_NAME = "경주 할 자동차 이름(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_NUMBER_OF_ATTEMPT = "시도할 회수";
 }
