14a480cb1b8898d87dee387ee34190bca7b60f95
daruken <>
2022-10-07 11:22:05 +0900

    feat: 시도 회수 입력에서 0이하의 음수가 입력되었을 때 예외 발생하도록 로직 추가

--- src/main/java/racingcar/model/race/wrap_variable/NumberOfAttemptMove.java
+++ src/main/java/racingcar/model/race/wrap_variable/NumberOfAttemptMove.java
@@ -7,9 +7,8 @@ public class NumberOfAttemptMove {
 
     public NumberOfAttemptMove(String strNumber) {
         if (!isNumeric(strNumber)) {
-            throw new IllegalArgumentException(Message.ERROR_MESSAGE + "시도 회수는 숫자만 입력할 수 있습니다");
+            throw new IllegalArgumentException(Message.ERROR_MESSAGE + "시도 회수는 0을 초과하는 숫자만 입력할 수 있습니다");
         }
-
         this.numberOfAttemptMove = Integer.parseInt(strNumber);
     }
 
@@ -19,8 +18,8 @@ public class NumberOfAttemptMove {
 
     private static boolean isNumeric(String s) {
         try {
-            Double.parseDouble(s);
-            return true;
+            double num = Double.parseDouble(s);
+            return !(num <= 0);
         } catch (NumberFormatException e) {
             return false;
         }
